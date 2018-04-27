0b6536140681e736209f9832cf1215ea564446cf
Ilia Kebets <kebetsi@gmail.com>
2018-04-27 10:09:20 +0200

    write campaigns.get default test

--- test/app.test.js
+++ test/app.test.js
@@ -1,2 +1,34 @@
-const app = require('../src/app');
+const request = require('supertest');
+const should = require('should');
 
+const app = require('../lib/app');
+
+describe('app', () => {
+
+  describe('campaigns', () => {
+
+    describe('when querying campaigns', () => {
+
+      it('should return a list of campaigns', () => {
+
+        return request(app)
+          .get('/campaigns')
+          .set('Authorization', 'salut')
+          .expect(201)
+          .then(res => {
+            res.body.should.have.property('campaigns').which.is.a.Array();
+            const firstCampaign = res.body.handledJobs[0];
+            firstCampaign.should.be.a.Object();
+            firstCampaign.should.have.property('title').which.is.a.String();
+            firstCampaign.should.have.property('createdBy').which.is.a.String();
+            firstCampaign.should.have.property('permissions').which.is.a.String();
+            firstCampaign.should.have.property('created').which.is.a.String();
+          })
+
+      });
+
+    });
+
+  });
+
+});
