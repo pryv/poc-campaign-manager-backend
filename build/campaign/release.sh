63807a9326e0e63d07dc1da5c5110e3161b0332a
Ilia Kebets <kebetsi@gmail.com>
2018-09-24 08:47:12 +0200

    attempt to fix build/

--- build/campaign/release.sh
+++ build/campaign/release.sh
@@ -5,39 +5,39 @@ source /pd_build/buildconfig
 target_dir="/app/bin"
 log_dir="/app/log"
 conf_dir="/app/conf"
+data_dir="/app/data"
 
 header "Install application from release.tar"
 
 run mkdir -p $target_dir
 run chown app $target_dir
 
-# Unpack the application and run npm install.
-cd $target_dir
+# Unpack the application and run yarn install.
+pushd $target_dir
 run run tar -x --owner app -f \
   /pd_build/release.tar .
 
-echo "PYTHON VERSION"
-PYTHON=$(which python2.7) python --version
+run echo "PYTHON VERSION"
+PYTHON=$(which python2.7) run python --version
 
 PYTHON=$(which python2.7) run yarn install --prod --ignore-engines
 
-
-
-# Perform a release build of the source code. (-> lib)
-run npm run release
-rm -r source && mv lib source
+# Perform a release build of the source code. (-> dist)
+run yarn release
 
 # Copy the config file
 run mkdir -p $conf_dir && \
-  cp /pd_build/config/campaign.json $conf_dir/campaign.json
+  run cp /pd_build/config/campaign.json $conf_dir/campaign.json
 
 # Create the log dir
 run mkdir -p $log_dir && \
-  touch $log_dir/campaign.log && chown -R app:app $log_dir
+  run touch $log_dir/campaign.log && run chown -R app:app $log_dir
+
+# Create the data space
+run mkdir -p $data_dir && 
+  run chown -R app:app $data_dir
 
 # Install the script that runs the campaign service
 run mkdir /etc/service/campaign
 run cp /pd_build/runit/campaign /etc/service/campaign/run
 
-# Enable CRON for nightly job
-run rm /etc/service/cron/down
